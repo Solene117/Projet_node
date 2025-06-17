cc4525508491b897ba44b8f800b568208cf2ec0f
AllanLoisirs <allanloisirs@MacBook-Pro-de-Allan.local>
2025-06-17 09:17:28 +0200

    email service

--- casier_back/src/app.ts
+++ casier_back/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import testRoutes from './routes/test.routes'
 import lockerRoutes from './routes/locker.routes';
+import emailRoutes from './routes/email.routes'
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 
 app.use('/api/test', testRoutes);
 app.use('/api/lockers', lockerRoutes);
+app.use('/api/email', emailRoutes)
 
 export default app;
